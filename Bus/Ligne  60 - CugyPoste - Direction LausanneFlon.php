8cdb710cd8a1c67110676e55cec688f9658c9776
Julien Muggli <liozon@hotmail.com>
2017-09-28 07:46:23 +0200

    Removed unnecessary comments

--- Bus/Ligne  60 - CugyPoste - Direction LausanneFlon.php
+++ Bus/Ligne  60 - CugyPoste - Direction LausanneFlon.php
@@ -3,14 +3,11 @@
 // Getting data
 $url = 'http://transport.opendata.ch/v1/stationboard?id=8504178&limit=1&to=lausanneflon';   // Data from opendata.ch
 $ch = curl_init();
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// Will return the response, if false it prints the response
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, $url);
 $response = curl_exec($ch);
 curl_close($ch);
 $obj = json_decode($response);  // Object
-// $obj = json_decode($response, true); // Associative array
 
 //Navigating to the targeted infos 
 $stationboard = $obj->stationboard[0];  // Navigation to the station details and infos
@@ -19,8 +16,6 @@ $now = new DateTime();
 $now->setTimezone(new DateTimeZone('Europe/Zurich'));
 $nextDeparture = $stationboard->stop->departureTimestamp;   // Getting the departure timestamp info
 $dep = date_create_from_format('U', $nextDeparture);
-//$dep = new DateTime();
-//$dep->setTimestamp($nextDeparture);
 
 //Converting seconds to readable time
 $dep->setTimezone(new DateTimeZone('Europe/Zurich'));
@@ -30,7 +25,6 @@ $minutes += $diff->h * 60;
 $minutes += $diff->i;
 
 //Formatting the data for LaMetric Time
-//$text = $text . $minutes . 'm ' . $diff->s . 's';
 $text = 'Prochain départ dans ' . $minutes . ' minutes et ' . ($diff->s < 10 ? '0' . $diff->s : $diff->s) . ' secondes';
 $out = array('frames'=>array(
                array('index'=>0,
