c1eb4a63c00660df6452befb2d6c35fddb98f124
Julien Muggli <info@muggli.one>
2020-04-21 11:07:31 +0200

    Added thousand separator

--- Folding@home/foldingathome.php
+++ Folding@home/foldingathome.php
@@ -13,32 +13,32 @@ $data = [
                 ],
                 [
                     'index' => 1,
-                    'text'  => "#1: " . $json['donors'][0]['name'] . " - Total WUS: " . $json['donors'][0]['wus'] . " - Total credits: " . $json['donors'][0]['credit'] . " - Actual rank: " . $json['donors'][0]['rank'],
+                    'text'  => "#1: " . $json['donors'][0]['name'] . " - Total WUS: " . number_format($json['donors'][0]['wus'], 0, '', "'") . " - Total credits: " . number_format($json['donors'][0]['credit'], 0, '', "'") . " - Actual rank: " . number_format($json['donors'][0]['rank'], 0, '', "'"),
                     'icon'  => ''
                 ],
                 [
                     'index' => 2,
-                    'text'  => "#2: " . $json['donors'][1]['name'] . " - Total WUS: " . $json['donors'][1]['wus'] . " - Total credits: " . $json['donors'][1]['credit'] . " - Actual rank: " . $json['donors'][1]['rank'],
+                    'text'  => "#2: " . $json['donors'][1]['name'] . " - Total WUS: " . number_format($json['donors'][1]['wus'], 0, '', "'") . " - Total credits: " . number_format($json['donors'][1]['credit'], 0, '', "'") . " - Actual rank: " . number_format($json['donors'][1]['rank'], 0, '', "'"),
                     'icon'  => ''
                 ],
                 [
                     'index' => 3,
-                    'text'  => "#3: " . $json['donors'][2]['name'] . " - Total WUS: " . $json['donors'][2]['wus'] . " - Total credits: " . $json['donors'][2]['credit'] . " - Actual rank: " . $json['donors'][2]['rank'],
+                    'text'  => "#3: " . $json['donors'][2]['name'] . " - Total WUS: " . number_format($json['donors'][2]['wus'], 0, '', "'") . " - Total credits: " . number_format($json['donors'][2]['credit'], 0, '', "'") . " - Actual rank: " . number_format($json['donors'][2]['rank'], 0, '', "'"),
                     'icon'  => ''
                 ],
                 [
                     'index' => 4,
-                    'text'  => "#4: " . $json['donors'][3]['name'] . " - Total WUS: " . $json['donors'][3]['wus'] . " - Total credits: " . $json['donors'][3]['credit'] . " - Actual rank: " . $json['donors'][3]['rank'],
+                    'text'  => "#4: " . $json['donors'][3]['name'] . " - Total WUS: " . number_format($json['donors'][3]['wus'], 0, '', "'") . " - Total credits: " . number_format($json['donors'][3]['credit'], 0, '', "'") . " - Actual rank: " . number_format($json['donors'][3]['rank'], 0, '', "'"),
                     'icon'  => ''
                 ],
                 [
                     'index' => 5,
-                    'text'  => "#5: " . $json['donors'][4]['name'] . " - Total WUS: " . $json['donors'][4]['wus'] . " - Total credits: " . $json['donors'][4]['credit'] . " - Actual rank: " . $json['donors'][4]['rank'],
+                    'text'  => "#5: " . $json['donors'][4]['name'] . " - Total WUS: " . number_format($json['donors'][4]['wus'], 0, '', "'") . " - Total credits: " . number_format($json['donors'][4]['credit'], 0, '', "'") . " - Actual rank: " . number_format($json['donors'][4]['rank'], 0, '', "'"),
                     'icon'  => ''
                 ],
                 [
                     'index' => 6,
-                    'text'  => "#6: " . $json['donors'][5]['name'] . " - Total WUS: " . $json['donors'][5]['wus'] . " - Total credits: " . $json['donors'][5]['credit'] . " - Actual rank: " . $json['donors'][5]['rank'],
+                    'text'  => "#6: " . $json['donors'][5]['name'] . " - Total WUS: " . number_format($json['donors'][5]['wus'], 0, '', "'") . " - Total credits: " . number_format($json['donors'][5]['credit'], 0, '', "'") . " - Actual rank: " . number_format($json['donors'][5]['rank'], 0, '', "'"),
                     'icon'  => ''
                 ],
                 [
